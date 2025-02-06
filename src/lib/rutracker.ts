5602a55c51cafd2348aa7bba488c89c850d5a4c3
imonous <mykolassle@gmail.com>
2025-02-06 01:20:15 +0100

    add rutracker searching

--- src/lib/rutracker.ts
+++ src/lib/rutracker.ts
@@ -1,30 +1,63 @@
-import puppeteer, { Page } from 'puppeteer';
+import puppeteer, { Browser, Page } from 'puppeteer';
 
-async function initSession() {
-  const browser = await puppeteer.launch({headless: false});
-  const page = await browser.newPage();
+export default class RuTracker {
+  private rootURL: string = 'https://rutracker.org/';
+  private browser!: Browser;
+  private page!: Page; 
 
-  const username = process.env.RUTRACKER_USERNAME;
-  const password = process.env.RUTRACKER_PASSWORD;
-  if (username === undefined || password === undefined) {
-    throw new Error();
+  public constructor() {
   }
-  login(page, username, password);
+
+  public async initSession(): Promise<void> {
+    this.browser = await puppeteer.launch({headless: false});
+    this.page = await this.browser.newPage();
+
+    const username = process.env.RUTRACKER_USERNAME;
+    const password = process.env.RUTRACKER_PASSWORD;
+    if (username === undefined || password === undefined) {
+      throw new Error('Username or password empty');
+    }
+    await this.login(username, password);
+  }
+
+  public async resumeSession(): Promise<void> {
+  }
+
+  public async pauseSession(): Promise<void> {
+    this.browser.close();
+  }
+
+  private async login(username: string, password: string): Promise<void> {
+    if (await this.isSignedIn()) {
+      return;
+    }
+    await this.page.goto(this.rootURL);
+    await this.page.locator('a ::-p-text(Вход)').click();
+    await this.page.locator('#top-login-uname').fill(username);
+    await this.page.locator('#top-login-pwd').fill(password);
+    await this.page.locator('#top-login-btn').click();
+  }
+
+  private async isSignedIn(): Promise<boolean> {
+    if (this.page === undefined) {
+      return false;
+    }
+    return await this.page.$('#logged-in-username') !== null;
+  }
+
+  public async search(term: string): Promise<void> {
+    if (!await this.isSignedIn()) {
+      throw new Error('Not signed in');
+    }
+    const URL = this.rootURL + 'forum/tracker.php?nm=' + encodeURIComponent(term);
+    this.page.goto(URL);
+  }
 }
 
-async function resumeSession() {
+async function main() {
+  const rt = new RuTracker(); 
+  await rt.initSession();
+  await new Promise(resolve => setTimeout(resolve, 1500));
+  await rt.search('gattaca');
 }
-
-async function pauseSession() {
-
-}
-
-async function login(page: Page, username: string, password: string) {
-  await page.goto('https://rutracker.org/');
-  await page.locator('a ::-p-text(Вход)').click();
-  await page.locator('#top-login-uname').fill(username);
-  await page.locator('#top-login-pwd').fill(password);
-  await page.locator('#top-login-btn').click();
-}
-
-initSession();
+main();
